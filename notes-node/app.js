868c37d8ae49ec79dcb78eafdc5aa50994f9bfb4
JoanSants <joansants07@gmail.com>
2018-08-18 16:11:50 -0300

    Refactoring for Reusability

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,7 +4,7 @@ console.log("Starting app.js");
 //const _ = require('lodash');
 const yargs = require('yargs');
 
-const note = require('./note')
+const notes = require('./notes')
 
 const argv = yargs.argv;
 var command = argv._[0];
@@ -13,7 +13,13 @@ console.log(yargs.argv);
 if (command === 'list'){
     note.getAll();
 }else if(command === 'add'){
-    note.addNote(argv.title, argv.body);
+    note = notes.addNote(argv.title, argv.body);
+    if(note){
+        console.log("Note created");
+        console.log(`The title is ${note.title} and the text is ${note.body}`)
+    }else{
+        console.log("Invalid Title");
+    }
 }else if(command === 'remove'){
     note.removeNote(argv.title);
 }else if(command === 'read'){
